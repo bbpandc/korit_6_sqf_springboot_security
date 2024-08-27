1019b800018db7d7b71db7de4e62f213ef60a188
bbpandc <haeminsia@gmail.com>
2024-08-27 17:13:17 +0900

    로그인, 삭제

---
+++ SpringSecurityMybatis/src/main/java/com/study/SpringSecurityMybatis/controller/ExceptionControllerAdvice.java
@@ -0,0 +1,33 @@
+package com.study.SpringSecurityMybatis.controller;
+
+import com.study.SpringSecurityMybatis.exception.SignupException;
+import com.study.SpringSecurityMybatis.exception.ValidException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Set;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(ValidException.class)
+    public ResponseEntity<?> validException(ValidException e) {
+        return ResponseEntity.badRequest().body(e.getFieldErrors());
+    }
+
+    @ExceptionHandler(SignupException.class)
+    public ResponseEntity<?> signupException(SignupException e) {
+        return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> authenticationException(AuthenticationException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+}
